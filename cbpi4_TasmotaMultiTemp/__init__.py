fc4836aacfe31079da99ed0db0029b3b0eba9e89
avollkopf <43980694+avollkopf@users.noreply.github.com>
2022-05-24 07:05:35 +0200

    added actor

--- cbpi4_TasmotaMultiTemp/__init__.py
+++ cbpi4_TasmotaMultiTemp/__init__.py
@@ -7,6 +7,7 @@ from unittest.mock import MagicMock, patch
 import asyncio
 import random
 from cbpi.api import *
+from cbpi.api import parameters, Property, CBPiActor
 from cbpi.api.config import ConfigType
 from cbpi.api.base import CBPiBase
 import json
@@ -93,7 +94,67 @@ class TasmotaSensor(CBPiSensor):
         return dict(value=self.value)
 
 
+@parameters([Property.Text(label="topic", description="Topic for Tasmota Actor"), 
+             Property.Select(label="SamplingTime", options=[2,5,10],description="Time in seconds for power base interval (Default:10)")])
+class TasmotaMQTTPowerActor(CBPiActor):
+
+    # Custom property which can be configured by the user
+    @action("Set Power", parameters=[Property.Number(label="Power", configurable=True,description="Power Setting [0-100]")])
+    async def setpower(self,Power = 100 ,**kwargs):
+        self.power=int(Power)
+        if self.power < 0:
+            self.power = 0
+        if self.power > 100:
+            self.power = 100           
+        await self.set_power(self.power)      
+
+    async def on_start(self):
+        self.power = None
+        self.sampleTime = int(self.props.get("SamplingTime", 10)) 
+        self.topic = self.props.get("topic",None)
+        await self.publish_mqtt_message(self.topic, "off") 
+        self.state = False
+
+    async def publish_mqtt_message(self, topic, payload):
+        self.logger.info("Publish '{payload}' to '{topic}'".format(payload = payload, topic = self.topic))
+        await self.cbpi.satellite.publish(self.topic, payload, True)
+
+    async def on(self, power = None):
+        if power is not None:
+            self.power = power
+        else: 
+            self.power = 100
+        await self.publish_mqtt_message(self.topic, "on")        
+        self.state = True
+
+    async def off(self):
+        await self.publish_mqtt_message(self.topic, "off") 
+        self.state = False
+
+    def get_state(self):
+        return self.state
+    
+    async def run(self):
+        while self.running == True:
+            if self.state == True:
+                heating_time=self.sampleTime * (self.power / 100)
+                wait_time=self.sampleTime - heating_time
+                if heating_time > 0:
+                    await self.publish_mqtt_message(self.topic, "on") 
+                    await asyncio.sleep(heating_time)
+                if wait_time > 0:
+                    await self.publish_mqtt_message(self.topic, "off") 
+                    await asyncio.sleep(wait_time)
+            else:
+                await asyncio.sleep(1)
+
+    async def set_power(self, power):
+        self.power = power
+        await self.cbpi.actor.actor_update(self.id,power)
+        pass
+
 def setup(cbpi):
     cbpi.plugin.register("TasmotaSensor", TasmotaSensor)
+    cbpi.plugin.register("TasmotaMQTTPowerActor", TasmotaMQTTPowerActor)
     cbpi.plugin.register("TasmotaMQTTExtension", TasmotaMQTTExtension)
     pass
